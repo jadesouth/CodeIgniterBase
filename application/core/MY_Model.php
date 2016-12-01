c14135db164c88d64470e40c3233fbd8f69e64ae
wangnan <wangnan@anhao.com>
2016-12-01 13:01:29 +0830

    MY_Controller 和 MY_Model添加

---
+++ application/core/MY_Model.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Class MY_Model 应用的基模型
+ *
+ * @property CI_DB_query_builder $db
+ * @property CI_Loader           $load
+ *
+ * @author wangnan <wangnanphp@163.com>
+ * @date   2016-12-01 12:15:42
+ */
+class MY_Model extends CI_Model
+{
+
+}
